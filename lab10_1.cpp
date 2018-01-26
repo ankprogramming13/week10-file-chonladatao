1504fbbc05ebf0b97b15f61b3692fe8bb2f8757e
chonladatao <33056484+chonladatao@users.noreply.github.com>
2018-01-26 13:32:44 +0700

    การสร้างไฟล์
    อธิบายรายละเอียดของไฟล์

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+#include <iostream> //ผูกไฟล์
 #include <fstream>
 using namespace std;
 
 
 int main() {
-    ofstream myfile ("example.txt");
-    if (myfile.is_open())
+    ofstream myfile ("example.txt"); //ประกาศออปเจ็ก โดยการผูกเข้ากับ myfile และ ไฟล์ example.txt //
+    if (myfile.is_open()) // เช็กว่าเปิดได้หรือไม่//
     {
-        myfile << "This is a line.\n";
-        myfile << "This is another line.\n";
-        myfile.close();
+        myfile << "This is a line.\n";// การเขียนข้อมูลไปที่ myfile ลงในไฟล์ example.txt //
+        myfile << "This is another line.\n"; //เขียนข้อมูลใน myfile ว่า This is another line และการขึ้นบรรทัดใหม่//
+        myfile.close(); //ปิดไฟล์//
     }
     else cout << "Unable to open file";
 
-        return 0;
+        return 0; //จบโปรแกรม//
 }
